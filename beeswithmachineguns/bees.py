7154526e5f658b357c347d44a5732182f0db2281
Altonymous <chris@altonymous.com>
2013-05-15 18:51:06 -0300

    Cleaning up formatting.
    It looks like whoever added the _get_security_group_ids(...) method  used tabs while the rest of the project uses spaces.  I have converted the function to spaces.
    
    Also cleaned up the placement of subnet_id since it was indented with tabs as well.

--- beeswithmachineguns/bees.py
+++ beeswithmachineguns/bees.py
@@ -76,21 +76,21 @@ def _get_region(zone):
     return zone[:-1] # chop off the "d" in the "us-east-1d" to get the "Region"
 	
 def _get_security_group_ids(connection, security_group_names, subnet):
-	ids = []
-	# Since we cannot get security groups in a vpc by name, we get all security groups and parse them by name later
-	security_groups = connection.get_all_security_groups()
+    ids = []
+    # Since we cannot get security groups in a vpc by name, we get all security groups and parse them by name later
+    security_groups = connection.get_all_security_groups()
 	
-	# Parse the name of each security group and add the id of any match to the group list
-	for group in security_groups:
-		for name in security_group_names:
-			if group.name == name:
-				if subnet == None:
-                                        if group.vpc_id == None:
-                                                ids.append(group.id)
-                                elif group.vpc_id != None:
-                                        ids.append(group.id)
+    # Parse the name of each security group and add the id of any match to the group list
+    for group in security_groups:
+        for name in security_group_names:
+            if group.name == name:
+                if subnet == None:
+                    if group.vpc_id == None:
+                        ids.append(group.id)
+                    elif group.vpc_id != None:
+                        ids.append(group.id)
 		
-	return ids
+        return ids
 
 # Methods
 
@@ -126,7 +126,7 @@ def up(count, group, zone, image_id, instance_type, username, key_name, subnet):
         security_group_ids=_get_security_group_ids(ec2_connection, [group], subnet),
         instance_type=instance_type,
         placement=zone,
-		subnet_id=subnet)
+        subnet_id=subnet)
 
     print 'Waiting for bees to load their machine guns...'
 
