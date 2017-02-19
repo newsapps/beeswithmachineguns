8ec6929801a798c42820df078e36e0b0d8cd471a
Freedom Dumlao <fdumlao@Admins-MacBook-Pro.local>
2017-02-19 14:51:39 -0500

    cleaning up test files

--- beeswithmachineguns/bees_test.py
+++
@@ -1,9 +0,0 @@
-import bees
-import json
-
-sOptions = '{"post_file":"test1.json","contenttype":"application/json"}'
-options = json.loads(sOptions)
-
-bees.up(1,'bees-sg','us-east-1b','ami-5d155d37','t2.micro','ubuntu','commerce-bees','subnet-b12880e8')
-bees.attack('http://54.89.221.165',2,2,**options)
-bees.down()
